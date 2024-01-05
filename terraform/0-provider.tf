d13b1db090df510c5dc5a8c6a31ce76cd40268ca
Venki231 <50393817+venki231blr@users.noreply.github.com>
2024-01-05 14:05:39 -0500

    Update 0-provider.tf

--- terraform/0-provider.tf
+++ terraform/0-provider.tf
@@ -1,3 +1,4 @@
+# use export KUBE_CONFIG_PATH=~/.kube/config,  if you get an error saying "try setting KUBERNETES_MASTER environment variable"
 provider "helm" {
   kubernetes {
     config_path = "C:/Users/107425/.kube/config"
